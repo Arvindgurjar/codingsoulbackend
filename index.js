69e248bec9f0b519c53401922cade5bb0b1f1e6d
Arvindgurjar <88261937+Arvindgurjar@users.noreply.github.com>
2023-02-16 17:49:20 +0530

    initial

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+const dotenv = require("dotenv")
+const cookieParser = require("cookie-parser")
+const app =express();
+dotenv.config({path:"./Config.env"})
+require("./DataBaseConnection/Connection")
+const port = process.env.PORT || 5000;
+app.use(express.json())
+app.use(require("./Routers/Router"));
+app.use(cookieParser());
+
+app.listen(port,()=>{
+    console.log("Successful");
+}) 
