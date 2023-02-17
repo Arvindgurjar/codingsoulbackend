329f25be66a93529a35a5a8e1774982c5d346411
Arvindgurjar <88261937+Arvindgurjar@users.noreply.github.com>
2023-02-17 18:07:54 +0530

    k

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ const port = process.env.PORT || 5000;
 app.use(express.json())
 app.use(require("./Routers/Router"));
 app.use(cookieParser());
-app.use(cors())
+app.use(cors({
+    origin:"https://codingsoulfrontend.onrender.com",
+    methods:["GET","POST","PATCH","PUT","DELETE"]
+}))
 
 app.listen(port,()=>{
     console.log("Successful");
