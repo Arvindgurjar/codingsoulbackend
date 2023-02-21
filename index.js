1e973b42d7e1fd634b2cd0dae9dfc200b8ba7c13
Arvindgurjar <88261937+Arvindgurjar@users.noreply.github.com>
2023-02-21 11:03:36 +0530

    index.js update

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ const express = require("express");
 const path = require("path");
 const cors = require("cors")
 const dotenv = require("dotenv")
+
+
+
 const cookieParser = require("cookie-parser")
 const app =express();
 dotenv.config({path:"./Config.env"})
 require("./DataBaseConnection/Connection")
+app.use(cors());
 const port = process.env.PORT || 5000;
 app.use(express.json())
 app.use(require("./Routers/Router"));
 app.use(cookieParser());
-app.use(cors({
-    origin:"https://codingsoulfrontend.onrender.com",
-    methods:["GET","POST","PATCH","PUT","DELETE"]
-}))
 
 app.listen(port,()=>{
     console.log("Successful");
